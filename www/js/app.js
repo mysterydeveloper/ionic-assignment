d5de43bb3141538cb0f293a110d2b440a4ee2bd4
mysterydeveloper <manus.duggan@yahoo.com>
2015-04-29 20:57:58 +0100

    master
    the assignment is finished

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,73 @@
-angular.module('calorific', ['ionic'])
+angular.module('Calorie Counter', ['ionic', 'Calorie Counter.controllers', 'Calorie Counter.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+      		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+
+	.state('app', {
+		url: "/app",
+    	abstract: true,
+    	templateUrl: "templates/menu.html"
+	})
+
+	.state('app.home', {
+		url: "/home",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/home.html",
+				controller: 'HomeCtrl'
+			}
+		}
+	})
+  .state('app.about', {
+    url: "/about",
+    views: {
+			'menuContent': {
+				templateUrl: "templates/about.html",
+				
+      }
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+  })
+  .state('app.settings', {
+    url: "/settings",
+    views: {
+			'menuContent': {
+				templateUrl: "templates/settings.html",
+				controller: 'SettingsCtrl'
+      }
+    }
+  })
+     .state('app.add', {
+    url: "/add",
+    views: {
+			'menuContent': {
+				templateUrl: "templates/addCal.html",
+				controller: 'HomeCtrl'
+      }
+    }
+  })
+  .state('app.goal', {
+    url: "/goal",
+    views: {
+			'menuContent': {
+				templateUrl: "templates/goal.html",
+				controller: 'GoalCtrl'
+      }
     }
   });
-})
+	
+		// if none of the above states are matched, use this as the fallback
+		$urlRouterProvider.otherwise('/app/home');
+});
